0562f5953eca89aa6de003d4e3b3e35625ee7d20
RKCbas <sebashp2015@hotmail.com>
2025-12-06 12:44:47 -0600

    Corrected syntax errores

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -1,12 +1,23 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class SecurityHeaders
 {
-    public function handle($request, Closure $next)
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @param  Closure(Request): Response  $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
     {
+        /** @var Response $response */
         $response = $next($request);
 
         $response->headers->set('X-Frame-Options', 'DENY');
@@ -18,4 +29,3 @@ class SecurityHeaders
         return $response;
     }
 }
-
